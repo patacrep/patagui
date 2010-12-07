e8674ffabeab888e37a73f1e5540289418b1afaf
Romain Goffe <romain.goffe@gmail.com>
2010-12-07 20:38:45 +0100

    Revert "use default sb file to produce pdf"
    This reverts commit 47af188b7e9ec1691ef721df8782a5d6d4e3c489.

--- src/mainwindow.cc
+++ src/mainwindow.cc
@@ -879,14 +879,14 @@ void CMainWindow::save()
 {
   if (m_songbook->filename().isEmpty())
     {
-      //default songbook sb file
-      QString defaultFile( QString("%1/default.sb").arg(workingPath()) );
-      m_songbook->setFilename(defaultFile);
+      saveAs();
     }
-
-  updateSongsList();
-  m_songbook->save(m_songbook->filename());
-  updateTitle(m_songbook->filename());
+  else
+    {
+      updateSongsList();
+      m_songbook->save(m_songbook->filename());
+      updateTitle(m_songbook->filename());
+    }
 }
 //------------------------------------------------------------------------------
 void CMainWindow::saveAs()
