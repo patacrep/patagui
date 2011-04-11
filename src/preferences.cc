db6dcccceb92bec0b4e6c27aac06a9b6ed647526
Alexandre Dupas <alexandre.dupas@gmail.com>
2011-04-11 12:44:35 +0200

    Add the proper range for the port spin box

--- src/preferences.cc
+++ src/preferences.cc
@@ -371,6 +371,7 @@ NetworkPage::NetworkPage(QWidget *parent)
 {
   m_hostname = new QLineEdit;
   m_port = new QSpinBox;
+  m_port->setRange(0, 65535);
   m_user = new QLineEdit;
   m_password = new QLineEdit;
   m_password->setEchoMode(QLineEdit::Password);
