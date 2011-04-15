4b8c92432e65d56696d0c914f4a6fadda5ccc81a
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2011-04-15 11:53:30 +0200

    fix liscence in file not passing test
    	src/songbook-pannel.{cc,hh}
    	copyright from 2011 to 2009-2011

--- src/song-panel.cc
+++ src/song-panel.cc
@@ -1,5 +1,5 @@
-// Copyright (C) 2011, Romain Goffe <romain.goffe@gmail.com>
-// Copyright (C) 2011, Alexandre Dupas <alexandre.dupas@gmail.com>
+// Copyright (C) 2009-2011, Romain Goffe <romain.goffe@gmail.com>
+// Copyright (C) 2009-2011, Alexandre Dupas <alexandre.dupas@gmail.com>
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License as
