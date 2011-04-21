48686c27060880b89e62ad7a0bead752b7389178
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2011-04-21 23:32:25 +0200

    remove dmg from git, create script to automatize dmg creation
    	this small script need another bash script that con be found here
    	https://github.com/Carreau/yoursway-create-dmg
    	should only change things for mac os

---
+++ macos_specific/makedmg.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+# this script need 
+# https://github.com/Carreau/yoursway-create-dmg
+# to correctly create a .dmg
+rm -rf .tmp
+test -f Songbook-Client.dmg && rm Songbook-Client.dmg
+~/yoursway-create-dmg/create-dmg   \
+	--icon-size 96  \
+	--linktoappfolder \
+	--window-pos 413 295  \
+	--window-size 770 367  \
+	--icon Applications 527 187 \
+	--volname "Songbook Client v0.4"   \
+	--icon "Songbook-Client.app" 201 187  \
+	--background ./macos_specific/image_disque_backgroud.png  \
+	Songbook-Client.dmg ~/songbook-client/build/Songbook-Client.app
