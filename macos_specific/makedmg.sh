55d93af5053d3a1801ced97a4749f2965f52e817
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2011-09-29 08:14:26 +0200

    allow some argument for building nightly

--- macos_specific/makedmg.sh
+++ macos_specific/makedmg.sh
@@ -2,8 +2,10 @@
 # this script need 
 # https://github.com/Carreau/yoursway-create-dmg
 # to correctly create a .dmg
+if [ -z "$1" ]; then dmg_name="Songbook-CLient v0.X.x.dmg" ; else dmg_name="$1"; fi
 rm -rf .tmp
-test -f Songbook-Client.dmg && rm Songbook-Client.dmg
+test -f $dmg_name && rm $dmg_name
+echo "using "$dmg_name" as image disk name"
 ~/yoursway-create-dmg/create-dmg   \
 	--icon-size 96  \
 	--linktoappfolder \
@@ -13,4 +15,4 @@ test -f Songbook-Client.dmg && rm Songbook-Client.dmg
 	--volname "Songbook Client v0.5.1"   \
 	--icon "Songbook-Client.app" 201 187  \
 	--background ./macos_specific/image_disque_backgroud.png  \
-	Songbook-Client.dmg ~/songbook-client/build/Songbook-Client.app
+	$dmg_name ~/songbook-client/build/Songbook-Client.app
