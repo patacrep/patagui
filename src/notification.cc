ebbcb3790e45e895fa126197a538448679e382e0
Romain Goffe <romain.goffe@gmail.com>
2011-10-13 18:08:13 +0200

    reduce notification icon  to prevent odd behaviors with some themes

--- src/notification.cc
+++ src/notification.cc
@@ -37,7 +37,7 @@ CNotification::CNotification(QWidget* p)
 
   QIcon icon = QIcon::fromTheme("dialog-information", QIcon(":/tango/dialog-information"));
   QLabel* label = new QLabel;
-  label->setPixmap(icon.pixmap(64,64));
+  label->setPixmap(icon.pixmap(48,48));
 
   m_textEdit = new QTextEdit;
   m_textEdit->setReadOnly(true);
