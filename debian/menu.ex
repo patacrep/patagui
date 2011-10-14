b8ffc5307d0d50def105c09d1f64a36089ce1dcb
Romain Goffe <romain.goffe@gmail.com>
2011-10-14 15:48:04 +0200

    cleanup unecessary debian files

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(songbook-client):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="songbook-client" command="/usr/bin/songbook-client"
--- debian/songbook-client.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the songbook-client package
-#
-0 4	* * *	root	[ -x /usr/bin/songbook-client_maintenance ] && /usr/bin/songbook-client_maintenance
