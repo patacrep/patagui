2a16c1dc25f97752e175af0251cad80c7d9cbe84
Romain Goffe <romain.goffe@gmail.com>
2012-01-20 22:46:12 +0100

    cleanup

--- macos_specific/check.sh
+++ macos_specific/check.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
-	echo ' Avez vous vérifiez le numéro de version ?' 
-	pause 5
-	macdeployqt ../build/Songbook-Client.app/
-	cp -r /Library/Frameworks/Sparkle.framework/ ../build/Songbook-Client.app/Contents/Frameworks/
-	echo ' Vérifiez la présence de la clé publique '
-	ls ../build/Songbook-Client.app/Contents/Resources
+
+#Author: Carreau (M. Bussonnier)
+#Descritpion: assist script to deploy the app on mac
+
+if [ $# -ne 1 ];
+then
+    echo "Usage: $0 BUILD_DIR"
+    echo "     BUILD_DIR: build directory of the application"
+    echo "                such as ../build"
+    exit 1
+fi;
+
+if [ -d $build_dir ]; then
+    echo "Did you check the version number of the application?"
+    pause 5
+    macdeployqt $build_dir/build/Songbook-Client.app/
+    cp -r /Library/Frameworks/Sparkle.framework/ $build_dir/build/Songbook-Client.app/Contents/Frameworks/
+    echo "Please check that the public key exists"
+    ls ../build/Songbook-Client.app/Contents/Resources
+else 
+    echo "Cannot find build directory : $build_dir"
+fi 
+
