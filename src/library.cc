645c84807c62ba4aed218788542765a790c1d78b
Romain Goffe <romain.goffe@gmail.com>
2012-06-01 22:37:28 +0200

    status tip when updating the library

--- src/library.cc
+++ src/library.cc
@@ -239,6 +239,7 @@ void CLibrary::update()
   while(it.hasNext())
     paths.append(it.next());
 
+  m_parent->statusBar()->showMessage(tr("Updating the library..."));
   m_parent->progressBar()->setCancelable(false);
   m_parent->progressBar()->setTextVisible(true);
   m_parent->progressBar()->setRange(0, paths.size());
@@ -260,7 +261,7 @@ void CLibrary::update()
   m_parent->progressBar()->setTextVisible(false);
   m_parent->progressBar()->setRange(0, 0);
   m_parent->progressBar()->hide();
-  m_parent->statusBar()->showMessage(tr("Song database updated."));
+  m_parent->statusBar()->showMessage(tr("Library updated."));
   emit(wasModified());
 }
 
