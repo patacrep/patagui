2a36124d6009443c39ec08361418d6a9ad9729f7
Romain Goffe <romain.goffe@gmail.com>
2012-07-14 18:22:30 +0200

    move chord model in its own files

--- src/diagram-area.hh
+++ src/diagram-area.hh
@@ -21,156 +21,18 @@
 
 
 #include <QWidget>
-#include <QAbstractTableModel>
 #include <QModelIndex>
 #include <QString>
 #include <QList>
 #include <QPoint>
 #include <QVector>
-#include <QMap>
 
 #include "chord.hh"
-
-
-/*!
-  \file diagram-area.hh
-  \class CTableDiagram
-  \brief CTableDiagram is a table model that contains CChord objects.
-
-  A CTableDiagram presents data on a grid where the number of rows or
-  columns is specified with setRowCount() or setColumnCount(). Adding
-  a new data element (CChord) to the model does not require
-  indicating its position on the grid as this is automatically
-  computed.
-
-  The following example defines a model that may have several columns
-  but a single row:
-
-  \code
-  CTableDiagram *model = new CTableDiagram;
-  model->setRowCount(1);
-  model->addItem("\gtab{C}{X32010}");
-  model->addItem("\gtab{Am}{X02210}");
-  model->addItem("\gtab{G}{X02210}");
-  model->addItem("\gtab{D}{XX0232}");
-  \endcode
-
-  Intuitively, the example above behaves like a QHBoxLayout whereas in
-  this second example, the model presents data on 3 columns but any
-  number of rows (thus, the D chord is displayed on the second row):
-
-  \code
-  CTableDiagram *model = new CTableDiagram;
-  model->setRowCount(3);
-  model->addItem("\gtab{C}{X32010}");
-  model->addItem("\gtab{Am}{X02210}");
-  model->addItem("\gtab{G}{X02210}");
-  model->addItem("\gtab{D}{XX0232}");
-  \endcode
-*/
-class CTableDiagram : public QAbstractTableModel
-{
-  Q_OBJECT
-
-  public:
-  /*!
-    \enum DiagramRoles
-    Each CChord object the model has a set of data elements associated with it, each with its own role.
-    The roles are used by the view to indicate to the model which type of data it needs.
-  */
-  enum DiagramRoles {
-    NameRole = Qt::UserRole + 1, /*!< the name of the chord.*/
-    StringsRole = Qt::UserRole + 2, /*!< the strings sequence of the chord.*/
-    TypeRole = Qt::UserRole + 3, /*!< the instrument of the chord.*/
-    ImportantRole = Qt::UserRole + 4, /*!< whether the chord is important.*/
-    MaxRole = ImportantRole
-  };
-
-  /// Constructor.
-  CTableDiagram(QWidget *parent=0);
-
-  /// Destructor.
-  ~CTableDiagram();
-
-  /*!
-    Returns the number of columns.
-    \sa setColumnCount, rowCount, setRowCount
-  */
-  virtual int columnCount(const QModelIndex & index = QModelIndex()) const;
-
-  /*!
-    Sets the number of columns to \a value.
-    Calling this method defines a "fixed" column count mode which means
-    that any item that is added afterwards will start a new row if necessary.
-    \sa columnCount, rowCount, setRowCount
-  */
-  virtual void setColumnCount(int value);
-
-  /*!
-    Returns the number of rows.
-    \sa setRowCount, columnCount, setColumnCount
-  */
-  virtual int rowCount(const QModelIndex & index = QModelIndex()) const;
-
-    /*!
-    Sets the number of rows to \a value.
-    Calling this method defines a "fixed" row count mode which means
-    that any item that is added afterwards will start a new column if necessary.
-    \sa rowCount, columnCount, setColumnCount
-  */
-  virtual void setRowCount(int value);
-
-  /* drag and drop */
-  Qt::DropActions supportedDropActions() const;
-  Qt::DropActions supportedDragActions() const;
-  Qt::ItemFlags flags(const QModelIndex &index) const;
-  QStringList mimeTypes() const;
-  QMimeData * mimeData(const QModelIndexList &indexes) const;
-  bool dropMimeData(const QMimeData *data, Qt::DropAction action,
-		    int row, int column, const QModelIndex &parent);
-
-  /*!
-    Reimplements QAbstractTableModel::data().
-    Returns the item (CChord object) at position \a index according to the role \a role.
-    \sa setData
-   */
-  QVariant data ( const QModelIndex & index, int role = Qt::DisplayRole ) const;
-
-  /*!
-    Reimplements QAbstractTableModel::setData().
-    Sets the value \a value of the item (CChord object) at position \a index for the role \a role.
-    \sa data
-   */
-  bool setData(const QModelIndex & index, const QVariant & value, int role = Qt::EditRole);
-
-
-  /*!
-    Returns the CChord object at position \a index.
-    \sa data, setData
-   */
-  CChord * getDiagram(const QModelIndex & index) const;
-
-public slots:
-  void insertItem(const QModelIndex & index, const QString & value);
-  void removeItem(const QModelIndex & index);
-  void addItem(const QString & value);
-
-private:
-  QModelIndex indexFromPosition(int position);
-  int positionFromIndex(const QModelIndex & index) const;
-
-private:
-  bool m_fixedColumnCount;
-  bool m_fixedRowCount;
-  int m_columnCount;
-  int m_rowCount;
-  QVector<CChord*> m_data;
-};
-
 
 class QPushButton;
 class QTableView;
 class QSortFilterProxyModel;
+class CTableDiagram;
 
 /*!
   \file diagram-area.hh
