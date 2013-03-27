3d2b288cb250a4f438ff4aec0541a6defabfbee5
Romain Goffe <romain.goffe@gmail.com>
2013-03-27 22:56:04 +0100

    fix fallback icons for go-up / go-down in search widget

--- src/search-widget.cc
+++ src/search-widget.cc
@@ -42,12 +42,12 @@ CSearchWidget::CSearchWidget(QWidget *parent)
 
   m_findPrevButton->setFlat(true);
   m_findPrevButton->setMaximumWidth(20);
-  m_findPrevButton->setIcon(QIcon::fromTheme("go-up", QIcon(":/icons/tango/48x48/actions/go-up.png")));
+  m_findPrevButton->setIcon(QIcon::fromTheme("go-up", QIcon(":/icons/songbook/48x48/fold.png")));
   connect(m_findPrevButton, SIGNAL(clicked()), SLOT(find()));
 
   m_findNextButton->setFlat(true);
   m_findNextButton->setMaximumWidth(20);
-  m_findNextButton->setIcon(QIcon::fromTheme("go-down", QIcon(":/icons/tango/48x48/actions/go-down.png")));
+  m_findNextButton->setIcon(QIcon::fromTheme("go-down", QIcon(":/icons/songbook/48x48/unfold.png")));
   connect(m_findNextButton, SIGNAL(clicked()), SLOT(find()));
 
   QPushButton *closeButton = new QPushButton(this);
